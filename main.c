7adefb268a5517faf62ec79e9adbc285348dcaae
Cristian Musat <cristutz00@gmail.com>
2024-02-25 15:40:04 +0200

    test gpg
    Signed-off-by: Cristian Musat <cristutz00@gmail.com>

--- main.c
+++ main.c
@@ -5,24 +5,53 @@
 
 #include "my_header.h"
 #include "structs.h"
+#include "macro_log.h"
+
+// int __SIZE__ = 10;
 
 int main(void)
 {
-    int values[10] = {2, 4, 5, 6, 7, 8, 9, 1, 3, 0};
+    int values[__SIZE__] = {2, 4, 5, 6, 7, 8, 9, 1, 3, 0};
 
-    struct Element *list;
+    // struct Element *ptrEl = (struct Element *)malloc(sizeof(struct Element));
+    // struct Element *start = ptrEl;
 
-    list = malloc(sizeof(struct Element) * 19);
+    // for (int i = 0; i < __SIZE__; ++i)
+    // {
+    //     ptrEl->value = values[i];
+    //     ptrEl->next = (struct Element *)malloc(sizeof(struct Element));
+    //     ptrEl = i < (__SIZE__ - 1) ? ptrEl->next : NULL;
+    // }
 
-    for (int i = 0; i < 10; i++)
+    // print_n_values(5, start);
+    // // print_n_values(2, start);
+
+    // // free(start);
+    // free_list(start);
+    // printf("\tstart este %p\n", *start);
+    // printf("\tintiial este %p\n", ptrEl);
+
+    struct Element *curr = NULL;
+    struct Element *prev;
+    struct Element *start = (struct Element *)malloc(sizeof(struct Element));
+
+    // curr = NULL;
+    prev = start;
+
+    for (int i = 0; i < 5; ++i)
     {
-        list[i].value = values[i];
-        list[i].next = &(list[i + 1]);
-    };
+        curr = (struct Element *)malloc(sizeof(struct Element));
+        prev->value = values[i];
+        prev->next = curr;
 
-    print_n_values(5, list);
-    print_n_values(7, list);
+        prev = curr;
+    }
 
-    free(list);
+    // curr = start;
+    // while (curr) {
+
+    // }
+    printf("%d", start->next->value);
+
     return 0;
 };
